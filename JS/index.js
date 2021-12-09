f3c442fa30efa48e6ad5b4bf29b4bde02ef7219e
micfol <91289377+micfol@users.noreply.github.com>
2021-12-09 20:37:11 +0000

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,25 @@
+var controller, rectangle, loop;
+
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+const width = canvas.width;
+const height = canvas.height
+
+let health = document.getElementById("health")
+health.value -= 0;
+// const player1 = new Player();
+
+// function startGame(){}
+
+// function clearCanvas(){}
+
+// function updateAll(){}
+
+
+
+
+
+
+
+
