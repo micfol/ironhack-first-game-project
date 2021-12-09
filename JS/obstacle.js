d137335e24af33805f86def23fec91564245e367
Michael Foley <foley.p.michael@gmail.com>
2021-12-09 14:34:26 +0000

    added new files, and got the canvas to have a rectangle player icon, that moves back and forth, restricted within the confines of the canvas frame.

---
+++ JS/obstacle.js
@@ -0,0 +1,7 @@
+// class Flags () {}
+
+
+// class Tourist () {}
+
+
+// class PickPocket () {}
---
+++ JS/player.js
@@ -0,0 +1,31 @@
+// class Player {
+//     constructor() {
+//         this.x = canvas.width - 550;
+//         this.y = canvas.height - 100;
+//         this.dx = 25;
+//         this.dy = 30;
+        
+//     }
+
+//     moveLeft(){
+//         if (this.x > 55){
+//             this.x -= this.dx;
+//         };
+//     }
+
+//     moveRight(){
+//         if (this.x < 400){
+//             this.x += this.dx;
+//         };
+//     };
+
+//     moveUp(){
+//         this.y += this.dy;
+//         };
+    
+
+//     drawPlayer () {
+//         ctx.fillStyle = this.color;
+//         ctx.fillRect(this.x, this.y, this.width, this.height);
+//       }
+// };
